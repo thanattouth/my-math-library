e56f47b4746eb7a8682761c179522fa05487867c
thanattouth <uzumaki1747@gmail.com>
2023-11-11 11:22:15 +0700

    ForEach

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 function sum(a, b) {
 	if (Array.isArray(a)) {
-		let total = 0
-		for (let i = 0; i < a.length; i++) {
-			total += a[i]
-		}
-		return total
-	} 
+		let sum = 0
+		a.forEach((i) => (sum += i))
+		return sum
+	}
 	else if (typeof a === 'number' && typeof b === 'number') {
 		return a + b
 	}
