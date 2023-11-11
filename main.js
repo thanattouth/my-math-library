5d1944801aa0da3e988cf2e51a7a4770d701f1e0
thanattouth <uzumaki1747@gmail.com>
2023-11-11 11:25:25 +0700

    reduce

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 function sum(a, b) {
 	if (Array.isArray(a)) {
-		let sum = 0
-		a.forEach((i) => (sum += i))
-		return sum
+		return a.reduce((acc, n) => acc + n, 0)
 	}
 	else if (typeof a === 'number' && typeof b === 'number') {
 		return a + b
