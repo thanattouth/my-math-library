8ea7bc89775ede0ec42237b580b9f0a9b919b6bd
thanattouth <uzumaki1747@gmail.com>
2023-11-11 11:15:47 +0700

    Add sum array functionality

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function sum(a, b) {
-	return a + b
+	if (Array.isArray(a)) {
+		let total = 0
+		for (let i = 0; i < a.length; i++) {
+			total += a[i]
+		}
+		return total
+	} 
+	else if (typeof a === 'number' && typeof b === 'number') {
+		return a + b
+	}
 }
 
 module.exports = {
